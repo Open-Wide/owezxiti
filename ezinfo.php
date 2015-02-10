258dd6d000ec1a18521aeac26ce0be56dcd7c68a
Simon Alberny <simon.alberny@openwide.fr>
2015-02-10 11:56:20 +0100

    Files for eZ infos about the extension added

---
+++ ezinfo.php
@@ -0,0 +1,18 @@
+<?php
+
+class owezxitiInfo
+{
+
+    static function info()
+    {
+        return array( 'Name'             => 'OWeZXiTi',
+                      'Version'          => 1,
+                      'eZ version'       => '4.x',
+                      'Copyright'        => '',
+                      'License'          => 'GNU General Public License v2.0',
+                      'More Information' => ''
+                    );
+    }
+
+}
+?>
